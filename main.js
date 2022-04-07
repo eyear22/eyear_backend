e0826441a203ac0b114f99734f40ee2aceda01b4
eminentecero <rapael860429@naver.com>
2022-04-07 18:00:47 +0900

    #3 - 업로드 기능 구현 완료

--- main.js
+++ main.js
@@ -5,16 +5,65 @@ const joinRouter = require('./routes/join');
 const userRouter = require('./routes/user');
 const businessRouter = require('./routes/business');
 const noticeRouter = require('./routes/notice');
+const multer = require('multer');
+const path = require('path');
 
 const app = express(); // app생성
 const port = 5000;
+const fs = require('fs');
 
+// DB 연결
 app.listen(port, () => console.log(`${port}포트입니다.`));
 // 몽구스 연결
 dbconnect();
+
+// 파일 서버 업로드 api
+try{
+    fs.readdirSync('uploads');
+} catch (error){
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    // 폴더 생성
+    fs.mkdirSync('uploads');
+}
+
+const upload = multer({
+    storage: multer.diskStorage({
+        // 저장하는 곳을 지정
+        destination(req, file, done){
+            done(null, 'uploads/');
+        },
+        // 저장할 파일 이름 지정
+        filename(req, file, done){
+            const ext = path.extname(file.originalname);
+            console.log("파일명 확인" + ext);
+            // 파일명이 겹치는 것을 막기 위해 Date.now 사용
+            done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+    }),
+    // 파일 크기를 5MB로 제한
+    //limits: {fileSize: 5 * 1024 * 1024},
+});
+
+app.get('/upload', (req, res) => {
+    res.sendFile(path.join(__dirname, 'upload.html'));
+});
+
+app.post('/upload',
+    upload.fields([{name: 'many'}]),
+    (req, res) => {
+        console.log(req.files, req.body);
+        res.send('ok');
+    },
+);
+
+app.get('/', (req, res, next) => {
+    console.log('Get / 요청시에만 실행됩니다.');
+});
+
 
 app.use('/', mainRouter);
 app.use('/join', joinRouter);
 app.use('/user', userRouter);
 app.use('/business', businessRouter);
 app.use('/notice', noticeRouter);
+
