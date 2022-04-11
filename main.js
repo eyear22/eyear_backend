2af2d03e05cb7f4c41ec4c0cdfb57f5ac58f07ac
naerim <naerim1119@gmail.com>
2022-04-11 14:20:48 +0900

    [CHORE] 비디오 모듈 생성

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 const express = require('express'); // express 임포트
-const multer = require('multer');
-const path = require('path');
 const cors = require('cors');
-const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const dbconnect = require('./models');
@@ -11,6 +8,7 @@ const joinRouter = require('./routes/join');
 const userRouter = require('./routes/user');
 const businessRouter = require('./routes/business');
 const noticeRouter = require('./routes/notice');
+const videoRouter = require('./routes/video');
 
 const app = express(); // app생성
 const port = 5000;
@@ -24,43 +22,6 @@ app.listen(port, () => console.log(`${port}포트입니다.`));
 // 몽구스 연결
 dbconnect();
 
-// 파일 서버 업로드 api
-try {
-  fs.readdirSync('uploads');
-} catch (error) {
-  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-  // 폴더 생성
-  fs.mkdirSync('uploads');
-}
-
-const upload = multer({
-  storage: multer.diskStorage({
-    // 저장하는 곳을 지정
-    destination(req, file, done) {
-      done(null, 'uploads/');
-    },
-    // 저장할 파일 이름 지정
-    filename(req, file, done) {
-      console.log(file.originalname);
-      const ext = path.extname(file.originalname);
-      console.log('파일명 확인', ext);
-      // 파일명이 겹치는 것을 막기 위해 Date.now 사용
-      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
-    },
-  }),
-  // 파일 크기를 5MB로 제한
-  // limits: {fileSize: 5 * 1024 * 1024},
-});
-
-app.get('/upload', (req, res) => {
-  res.sendFile(path.join(__dirname, 'upload.html'));
-});
-
-app.post('/upload', upload.array('file'), (req, res) => {
-  console.log(req.files);
-  res.send('ok');
-});
-
 app.get('/', () => {
   console.log('Get / 요청시에만 실행됩니다.');
 });
@@ -70,3 +31,4 @@ app.use('/join', joinRouter);
 app.use('/user', userRouter);
 app.use('/business', businessRouter);
 app.use('/notice', noticeRouter);
+app.use('/video', videoRouter);
--- routes/video.js
+++ routes/video.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+// 파일 서버 업로드 api
+try {
+  fs.readdirSync('uploads');
+} catch (error) {
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+  // 폴더 생성
+  fs.mkdirSync('uploads');
+}
 
 const router = express.Router();
-const multer = require('multer');
 
 const upload = multer({
   storage: multer.diskStorage({
@@ -11,18 +22,21 @@ const upload = multer({
     },
     // 저장할 파일 이름 지정
     filename(req, file, done) {
+      console.log(file.originalname);
       const ext = path.extname(file.originalname);
-      console.log('파일명 확인' + ext);
+      console.log('파일명 확인', ext);
       // 파일명이 겹치는 것을 막기 위해 Date.now 사용
       done(null, path.basename(file.originalname, ext) + Date.now() + ext);
     },
   }),
+  // 파일 크기를 5MB로 제한
+  // limits: {fileSize: 5 * 1024 * 1024},
 });
 
-router.post('/uploadfiles', upload.single('file'), (req, res) => {
+router.post('/upload', upload.array('file'), (req, res) => {
   // 비디오를 서버에 저장
-  console.log(req.files, req.body);
-  res.send('success');
+  console.log(req.files);
+  res.send('ok');
 });
 
 module.exports = router;
