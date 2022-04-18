40ec1952f2a29a678efb243a49c87073da50642b
eminentecero <rapael860429@naver.com>
2022-04-18 22:43:40 +0900

    #6 - STT 연결 성공

--- cloud/cloudstorage.js
+++ cloud/cloudstorage.js
@@ -2,37 +2,38 @@
 
 // Imports the Google Cloud client library
 const Speech = require('@google-cloud/speech');
-const fs = require('fs');
 
 // Creates a client
 const client = new Speech.SpeechClient();
-async function quickstart() {
   // The path to the remote LINEAR16 file
-  //const gcsUri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw';
-  const filename = '../uploads/nextlevel.1650285151213.mp3';
-
+  const gcsUri = 'gs://eyear_speech/nextlevel.1650285151213.mp3';
+  async function quickstart() {
   // The audio file's encoding, sample rate in hertz, and BCP-47 language code
   const audio = {
     //uri: gcsUri,
     // 원래 cloud에 음성 파일을 업로드 후 그걸 불러와서 하는 형식.
     // 일단 테스트용으로 실행중입니다.
-    content: fs.readFileSync(filename)
+    uri: gcsUri
   };
   const config = {
-    encoding: 'MP3',
-    sampleRateHertz: 16000,
-    languageCode: 'ko-KR',
+    encoding: 'mp3',
+    sampleRateHertz: 44100,
+    languageCode: 'ko-KR'
   };
+
   const request = {
     audio: audio,
     config: config,
   };
 
-  // Detects speech in the audio file
-  const [response] = await client.recognize(request);
-  const transcription = response.results
-    .map(result => result.alternatives[0].transcript)
-    .join('\n');
-  console.log(`Transcription: ${transcription}`);
+// Detects speech in the audio file. This creates a recognition job that you
+// can wait for now, or get its result later.
+const [operation] = await client.longRunningRecognize(request);
+// Get a Promise representation of the final result of the job
+const [response] = await operation.promise();
+const transcription = response.results
+  .map(result => result.alternatives[0].transcript)
+  .join('\n');
+console.log(`Transcription: ${transcription}`);
 }
 quickstart();
