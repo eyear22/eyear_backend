6e777b33aa600db1bc05a725609ae32922a8aa16
ParkChaerim <qkrdbsk28@naver.com>
2022-05-24 22:21:25 +0900

    [CHORE] 오타 수정

--- routes/video.js
+++ routes/video.js
@@ -4,8 +4,6 @@ const path = require('path');
 const fs = require('fs');
 const Video = require('../database/video_schema');
 const Image = require('../database/image_schema');
-const {Storage} = require('@google-cloud/storage');
-const storage = new Storage();
 const Text = require('../database/text_schema');
 //const ffmpeg = require('fluent-ffmpeg');
 const ffmpeg = require('ffmpeg');
@@ -47,48 +45,49 @@ router.post('/upload', upload.array('file'), async (req, res, next) => {
   try {
     await req.files.map((file) => {
       const type = file.mimetype.substr(file.mimetype.lastIndexOf('/') + 1); // 파일 type
-      const blob = bucket.file(Date.now() +"."+ type);
-      const blobStream = blob.createWriteStream();
-
-      blobStream.on('error', err => {
-        console.log("보내는데에 오류발생!");
-      // Create a new blob in the bucket and upload the file data.
-      // !!! file.originalname을 삭제하고 다른 걸로 대체할 방법 찾아보기.
       const blob = bucket.file(Date.now() + '.' + type);
-      console.log(file.originalname);
       const blobStream = blob.createWriteStream();
-      //blob.name = Date.now();
 
-      console.log('저장명' + blob.name);
       blobStream.on('error', (err) => {
         console.log('보내는데에 오류발생!');
-        next(err);
-      });
-      blobStream.on('finish', () => {
-        // The public URL can be used to directly access the file via HTTP.
-        const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
-      });
-        
-      // 업로드 실행
-      blobStream.end(file.buffer);
+        // Create a new blob in the bucket and upload the file data.
+        // !!! file.originalname을 삭제하고 다른 걸로 대체할 방법 찾아보기.
+        const blob = bucket.file(Date.now() + '.' + type);
+        console.log(file.originalname);
+        const blobStream = blob.createWriteStream();
+        //blob.name = Date.now();
 
-      if (type === 'mp4') {
-        // 동영상
-        Video.create({
-          video: `gs://${bucket.name}//${blob.name}`,
-          post_id: req.body.post_id,
+        console.log('저장명' + blob.name);
+        blobStream.on('error', (err) => {
+          console.log('보내는데에 오류발생!');
+          next(err);
         });
-      } else if (type === 'png' || 'jpeg' || 'jpg') {
-        // 이미지
-        Image.create({
-          video: `gs://${bucket.name}/${blob.name}`,
-          post_id: req.body.post_id,
+        blobStream.on('finish', () => {
+          // The public URL can be used to directly access the file via HTTP.
+          const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
         });
-      }
-      return type;
+
+        // 업로드 실행
+        blobStream.end(file.buffer);
+
+        if (type === 'mp4') {
+          // 동영상
+          Video.create({
+            video: `gs://${bucket.name}//${blob.name}`,
+            post_id: req.body.post_id,
+          });
+        } else if (type === 'png' || 'jpeg' || 'jpg') {
+          // 이미지
+          Image.create({
+            video: `gs://${bucket.name}/${blob.name}`,
+            post_id: req.body.post_id,
+          });
+        }
+        return type;
+      });
+
+      res.status(200).send('ok');
     });
-
-    res.status(200).send('ok');
   } catch (error) {
     console.log(err);
     next(err);
