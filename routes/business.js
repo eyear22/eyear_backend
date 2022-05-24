519aff7e9344be71f5c487c7b38db5a5c42a741b
ParkChaerim <qkrdbsk28@naver.com>
2022-05-24 23:01:20 +0900

    [FEAT] 환자 개인 리스트 GET api

--- routes/business.js
+++ routes/business.js
@@ -6,7 +6,7 @@ const Video = require('../database/video_schema');
 const Image = require('../database/image_schema');
 const Post = require('../database/post_schema');
 const User = require('../database/user_schema');
-const {Storage} = require('@google-cloud/storage');
+const { Storage } = require('@google-cloud/storage');
 const storage = new Storage();
 const Patient = require('../database/patient_schema');
 const router = express.Router();
@@ -66,22 +66,22 @@ router.post('/post', upload.array('many'), async (req, res, next) => {
     await req.files.map((file) => {
       // 여러 파일이 들어오므로 map() 사용
       const type = file.mimetype.substr(file.mimetype.lastIndexOf('/') + 1); // 파일 type
-      const blob = bucket.file(Date.now() +"."+ type);
+      const blob = bucket.file(Date.now() + '.' + type);
       console.log(file.originalname);
       const blobStream = blob.createWriteStream();
 
-      console.log("저장명" + blob.name);
-      blobStream.on('error', err => {
+      console.log('저장명' + blob.name);
+      blobStream.on('error', (err) => {
         next(err);
       });
       blobStream.on('finish', () => {
         // The public URL can be used to directly access the file via HTTP.
         const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
       });
-  
+
       // 업로드 실행
       blobStream.end(file.buffer);
-      
+
       if (type === 'mp4') {
         // 동영상
         Video.create({
@@ -112,6 +112,18 @@ router.get('/:hos_id/patientList', async (req, res, next) => {
       hos_id: req.params.hos_id,
     }).populate('hos_id');
     res.json(patientList);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:pat_id/userList', async (req, res, next) => {
+  if (!req) return;
+  try {
+    const userList = await User.find({
+      pat_id: req.params.pat_id,
+    });
+    res.json(userList);
   } catch (err) {
     next(err);
   }
