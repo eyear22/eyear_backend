1472b10dadff6adedbfbecab31f427ef922c7ae9
eminentecero <rapael860429@naver.com>
2022-06-03 00:17:07 +0900

    [FEAT] 회원가입에서 미리 필요한 데이터 불러오기

--- routes/join.js
+++ routes/join.js
@@ -1,12 +1,43 @@
 const express = require('express');
+const Hospital = require('../database/hospital_schema');
+const Patient = require('../database/patient_schema');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('회원가입 처음 페이지');
 });
 
-router.get('/user', (req, res) => {
-  res.send('개인 회원가입 페이지');
+router.get('/user', async (req, res, next) => {
+  // 병원 리스트 - 병원 아이디/병원이름
+  const Hospital_List = await Hospital.find({});
+
+  // 환자 리스트 - 고유번호/환자 아이디/환자 이름/환자 생년월일
+  const Patient_List = await Patient.find({});
+
+  const Hospital = Hospital_List.map((v) => ({
+    hospitalName: v.hos_name,
+    hospitalId: v._id,
+  }));
+
+  // TODO: 내림언니한테 변수 다시 변경하라고 말하기
+  const Patient = Patient_List.map((v) => ({
+    patientId: v._id,
+    patientNum: v.pat_number,
+    patientName: v.pat_name,
+    patientBirth: v.birth,
+  }));
+
+  const result = {
+    hospital: Hospital,
+    patient: Patient
+  };
+  res.send(result);
+
+  //res.send('개인 회원가입 페이지');
+});
+
+router.post('/user', async(req, res, next) => {
+
 });
 
 router.get('/business', (req, res) => {
