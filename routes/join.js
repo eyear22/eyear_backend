f4f3183547349c51945958cdd47ab3b42bbb8836
ParkChaerim <qkrdbsk28@naver.com>
2022-06-03 20:20:11 +0900

    [FEAT] user 아이디 중복 체크

--- routes/join.js
+++ routes/join.js
@@ -1,13 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const User = require('../database/user_schema');
+
 const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('회원가입 처음 페이지');
-});
-
-router.get('/user', (req, res) => {
-  res.send('개인 회원가입 페이지');
-});
 
 router.get('/business', (req, res) => {
   res.send('기관 회원가입 페이지');
@@ -15,6 +12,21 @@ router.get('/business', (req, res) => {
 
 router.get('/done', (req, res) => {
   res.send('회원가입 완료 페이지');
+});
+
+router.get('/user_id_check/:uid', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      uid: req.params.uid,
+    });
+    if (user === null) {
+      res.status(200).send('ok');
+    } else {
+      res.status(200).send('exit');
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
