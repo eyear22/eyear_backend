5fe84140b400439d129b10460ad6dc70bf8c3918
ParkChaerim <qkrdbsk28@naver.com>
2022-06-08 23:01:12 +0900

    [CHORE] 개인 편지 리스트 조회 api 수정

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../database/post_schema');
+
 
 router.get('/login', (req, res) => {
   res.send('로그인 페이지');
@@ -18,29 +18,4 @@ router.get('/modifyPwd', (req, res) => {
   res.send('비밀번호 변경 페이지');
 });
 
-router.get('/receive_list/:_id', async (req, res, next) => {
-  if (!req) return;
-  try {
-    const postList = await Post.find({
-      to: req.params._id,
-    });
-
-    res.json(postList);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get('/send_list/:_id', async (req, res, next) => {
-  if (!req) return;
-  try {
-    const postList = await Post.find({
-      from: req.params._id,
-    });
-
-    res.json(postList);
-  } catch (err) {
-    next(err);
-  }
-});
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,69 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../database/post_schema');
+const User = require('../database/user_schema');
+const Patient = require('../database/patient_schema');
 
-router.get('/receive', (req, res) => {
-  res.send('개인 받은 편지 확인 페이지');
+// 개인 받은 편지 리스트
+router.get('/receive_list/:_id', async (req, res, next) => {
+  if (!req) return;
+  try {
+    const postList = await Post.find({
+      to: req.params._id,
+    }).sort({ createdAt: -1 });
+
+    const result = [];
+    for (let i = 0; i < postList.length; i++) {
+      // eslint-disable-next-line
+      const patient = await Patient.findOne({ _id: postList[i].from });
+      if (patient !== null) {
+        const createdAt = JSON.stringify(postList[i].createdAt).substr(1, 10);
+        result[i] = {
+          _id: postList[i]._id,
+          title: postList[i].title,
+          content: postList[i].content,
+          createdAt: createdAt,
+          from: patient.pat_name,
+          to: postList[i].to,
+          check: postList[i].check,
+        };
+      }
+    }
+
+    res.send(result);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get('/send', (req, res) => {
-  res.send('개인 보낸 편지 확인 페이지');
+// 개인 보낸 편지 리스트
+router.get('/send_list/:_id', async (req, res, next) => {
+  if (!req) return;
+  try {
+    const postList = await Post.find({
+      from: req.params._id,
+    }).sort({ createdAt: -1 });
+    const result = [];
+    for (let i = 0; i < postList.length; i++) {
+      // eslint-disable-next-line
+      const patient = await Patient.findOne({ _id: postList[i].to });
+      if (patient !== null) {
+        const createdAt = JSON.stringify(postList[i].createdAt).substr(1, 10);
+        result[i] = {
+          _id: postList[i]._id,
+          title: postList[i].title,
+          content: postList[i].content,
+          createdAt: createdAt,
+          from: postList[i].from,
+          to: patient.pat_name,
+          check: postList[i].check,
+        };
+      }
+    }
+    res.send(result);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get('/post', (req, res) => {
