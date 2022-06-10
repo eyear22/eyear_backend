1dfdc231e5e57849a0ac99de2640ca9959133cf3
ParkChaerim <qkrdbsk28@naver.com>
2022-06-10 17:21:34 +0900

    [FEAT] 개인 환자 리스트 조회 api

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Post = require('../database/post_schema');
 const User = require('../database/user_schema');
 const Patient = require('../database/patient_schema');
+const Relation = require('../database/relationship_schema');
 
 // 개인 받은 편지 리스트
 router.get('/receive_list/:_id', async (req, res, next) => {
@@ -63,6 +64,27 @@ router.get('/send_list/:_id', async (req, res, next) => {
       }
     }
     res.send(result);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:user_id/patientList', async (req, res, next) => {
+  if (!req) return;
+  try {
+    const relation = await Relation.find({
+      user_id: req.params.user_id,
+    });
+
+    const patients = await Patient.find({
+      _id: { $in: relation.map((v) => v.pat_id) },
+    });
+
+    const patientList = patients.map((v) => ({
+      name: v.pat_name,
+      id: v._id,
+    }));
+    res.json(patientList);
   } catch (err) {
     next(err);
   }
